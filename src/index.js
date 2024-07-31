16446b5c8b1c8b3ddb30a89e317e854a72f82356
ill_Nino <innxfect@gmail.com>
2024-07-31 17:30:39 +0300

    feat: add function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
-module.exports = function reverse (n) {
-  
+module.exports = function getReversedNum(num) {
+    let result = 0;
+    while (num) {
+    result = result * 10 + (num % 10);
+    num = Math.floor(num / 10);
+    }
+    return result;
 }
